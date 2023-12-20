ba1cad38808dbee62385e11955d8d14111d23d72
Akex Mac <1115220867@qq.com>
2023-12-20 17:14:56 +0800

    交换方向乘子法

---
+++ src/gl_ADMM_dual.py
@@ -0,0 +1,51 @@
+import numpy as np
+import src.utils as utils
+
+# 交换方向乘子法（对偶问题）
+# 参考 http://faculty.bicmr.pku.edu.cn/~wenzw/optbook/lect/24-lect-admm-chhyx.pdf
+# 参考 http://faculty.bicmr.pku.edu.cn/~wenzw/optbook/pages/lasso_admm/LASSO_admm_primal.html
+
+def updateZ(ref, mu):
+    norm = np.linalg.norm(ref, axis=1, keepdims=True)
+    norm[norm < mu] = mu
+    return ref * (mu / norm)
+
+
+def gl_ADMM_dual(x0: np.ndarray, A: np.ndarray, b: np.ndarray, mu: float, opts:dict = {}):
+    opts = utils.ADMM_dual_optsInit(opts)
+    m, n = A.shape
+    _, l = b.shape
+    out = utils.outInit()
+    out['prim_hist'] = []
+    out['dual_hist'] = []
+
+    x = x0
+    sigma = opts['sigma']   # 二次罚函数系数
+    inv = np.linalg.inv(np.eye(m) + sigma * A @ A.T)
+    z = np.zeros((n, l))
+
+    for k in range(opts['maxit']):
+        y = inv @ (A @ x - sigma * A @ z  - b)
+        zp = z
+        z = updateZ(x / sigma - A.T @ y, mu)
+        inner_gap = np.linalg.norm(z - zp, 'fro')
+        # out['itr'] += 1
+        
+        x = x - sigma * (A.T @ y + z)
+
+        # f = 0.5 * np.linalg.norm(A @ x - b, ord='fro') ** 2 + mu * np.sum(np.linalg.norm(x, ord=2, axis=1))
+        f = utils.objFun(x, A, b, mu)
+        f_dual = 0.5 * np.linalg.norm(y, ord='fro') ** 2 + np.sum(y * b)
+        out['prim_hist'].append(f)
+        out['dual_hist'].append(f_dual)
+
+        if np.linalg.norm(A.T @ y + z) < opts['thre']:
+            break
+    
+    out['itr'] = k + 1
+    out['fval'] = out['prim_hist'][-1]
+    utils.logger.info(f"ADMM_dual: itr: {out['itr']}, fval: {out['fval']}")
+    utils.logger.info(f"ADMM_dual: len(out['prim_hist']): {len(out['prim_hist'])}")
+    out['iters'] = zip(range(out['itr']), out['prim_hist'])#, out['dual_hist'])
+    
+    return x, out['itr'], out
---
+++ src/gl_ADMM_primal.py
@@ -0,0 +1,48 @@
+import numpy as np
+import src.utils as utils
+
+# 交换方向乘子法（原问题）
+# 参考 http://faculty.bicmr.pku.edu.cn/~wenzw/optbook/pages/lasso_admm/demo_admm.html
+# 参考 http://faculty.bicmr.pku.edu.cn/~wenzw/optbook/pages/lasso_admm/LASSO_admm_primal.html
+
+def gl_ADMM_primal(x0: np.ndarray, A: np.ndarray, b: np.ndarray, mu: float, opts:dict = {}):
+    opts = utils.ADMM_primal_optsInit(opts)
+    _, n = A.shape
+    _, l = b.shape
+    out = utils.outInit()
+    out['prim_hist'] = []
+
+    x = x0
+    y = x0
+    z = np.zeros((n, l))
+
+    sigma = opts['sigma']   # 二次罚函数系数
+    inv = np.linalg.inv(sigma * np.eye(n) + A.T @ A) # 由于罚因子在算法的迭代过程中未变化，事先缓存 Cholesky 分解可以加速迭代过程。
+    ATb = A.T @ b
+
+    for k in range(opts['maxit']):
+        x = inv @ (sigma * y + ATb - z)
+        y0 = y
+        y = utils.prox(x + z / sigma, mu / sigma)
+        z = z + sigma * (x - y)
+
+        primal_sat = np.linalg.norm(x - y)
+        dual_sat = np.linalg.norm(y0 - y)
+        # f = 0.5 * np.linalg.norm(A @ x - b, ord='fro') ** 2 + mu * np.sum(np.linalg.norm(x, ord=2, axis=1))
+        f = utils.objFun(x, A, b, mu)
+        out['prim_hist'].append(f)
+        # out['itr'] += 1
+
+        if primal_sat < opts['thre'] and dual_sat < opts['thre']:
+            break
+    
+    utils.logger.info(f"k: {k}")
+    out['itr'] = k + 1
+    out['fval'] = f
+    utils.logger.info(f"ADMM_primal: itr: {out['itr']}, fval: {out['fval']}")
+    utils.logger.info(f"ADMM_primal: len(out['prim_hist']): {len(out['prim_hist'])}")
+    out['iters'] = zip(range(out['itr']), out['prim_hist'])
+
+    return x, out['itr'], out
+
+
